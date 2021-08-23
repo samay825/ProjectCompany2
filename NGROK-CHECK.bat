7e44f9e9cadba8cb56f36971569aaae4b6a60018
Simple hacker <87126670+simplehacker01@users.noreply.github.com>
2021-08-23 08:12:10 +0530

    Update NGROK-CHECK.bat

--- NGROK-CHECK.bat
+++ NGROK-CHECK.bat
@@ -1,13 +1,13 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Azure VM" > out.txt 2>&1
+net config server /srvcomment:"Simple Hacker1 VPS" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 curl -O https://raw.githubusercontent.com/vongocbao/WindowsRDP/main/DisablePasswordComplexity.ps1 > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\Fast Config VPS.exe" https://raw.githubusercontent.com/vongocbao/WindowsRDP/main/FastConfigVPS_v5.1.exe > out.txt 2>&1
 curl -o "C:\Users\Public\Desktop\EVKey Vietnamese Keyboard.exe" https://raw.githubusercontent.com/vongocbao/WindowsRDP/main/EVKey64.exe > out.txt 2>&1
-net user administrator Tharusha123. /add >nul
-net localgroup administrators administrator /add >nul
-net user administrator /active:yes >nul
+net user Simplehackerr @#Simple@. /add >nul
+net localgroup administrators Simplehackerr /add >nul
+net user Simplehackerr /active:yes >nul
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
